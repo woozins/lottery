faf1405251c0169f4663fe5acdb9183e8a77408d
woozi <86119975+woozins@users.noreply.github.com>
2024-08-26 09:15:38 +0900

    240826

---
+++ script_model3_11.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+#SBATCH --time=4-00:00:00 
+#SBATCH --nodes=1 
+#SBATCH --nodelist=n05
+#SBATCH --ntasks=1 
+#SBATCH --cpus-per-task=7
+#SBATCH --job-name="lottery_simulation3_11"
+#SBATCH --mail-user=cowzin@snu.ac.kr
+#SBATCH --mail-type=ALL
+#SBATCH --output=out/py-%x.%j.out
+#SBATCH --error=out/py-%x.%j.err
+
+module python3
+
+srun python3 /home/cowzin/code/simulation_model3_11.py 
+
--- script_model3_17.sh
+++ script_model3_17.sh
@@ -2,14 +2,15 @@
 #
 #SBATCH --time=4-00:00:00 
 #SBATCH --nodes=1 
-#SBATCH --nodelist=n06
+#SBATCH --nodelist=n05
 #SBATCH --ntasks=1 
-#SBATCH --cpus-per-task=50
-#SBATCH --job-name="lottery_simulation3_1"
+#SBATCH --cpus-per-task=7
+#SBATCH --job-name="lottery_simulation3_17"
 #SBATCH --mail-user=cowzin@snu.ac.kr
 #SBATCH --mail-type=ALL
 #SBATCH --output=out/py-%x.%j.out
 #SBATCH --error=out/py-%x.%j.err
 
 module python3
-srun python3 /home/cowzin/code/simulation_model3_1.py
+
+srun python3 /home/cowzin/code/simulation_model3_17.py
