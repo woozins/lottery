2490a5523107d17c1257d81332baa61c397c7859
woozi <86119975+woozins@users.noreply.github.com>
2024-09-02 08:59:11 +0900

    240902

--- script_model_gpu.sh
+++ script_model_gpu.sh
@@ -2,13 +2,13 @@
 #
 #SBATCH --time=4-00:00:00 
 #SBATCH --nodes=1 
-#SBATCH --nodelist=n06
+#SBATCH --nodelist=n01
 #SBATCH --ntasks=1 
-#SBATCH --cpus-per-task=50
+#SBATCH --cpus-per-task=10
 #SBATCH --job-name="lottery_simulation_gpu"
 #SBATCH --mail-user=cowzin@snu.ac.kr
 #SBATCH --mail-type=ALL
-#SBATCH --gres=gpu:100
+#SBATCH --gres=gpu:5
 #SBATCH --output=out/py-%x.%j.out
 #SBATCH --error=out/py-%x.%j.err
 
