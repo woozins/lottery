55d0cb7127fbb4e6e4b4dcfdfcbc7d4ff0bae153
woojin_99 <86119975+WOOZi-n@users.noreply.github.com>
2024-09-20 14:48:17 +0900

    240920_3

--- script_model6_nonrandom.sh
+++ script_model6_nonrandom.sh
@@ -2,14 +2,14 @@
 #
 #SBATCH --time=4-00:00:00 
 #SBATCH --nodes=1 
-#SBATCH --nodelist=n06
+#SBATCH --nodelist=n05
 #SBATCH --ntasks=1 
-#SBATCH --cpus-per-task=50
-#SBATCH --job-name="lottery_simulation_5_diffF"
+#SBATCH --cpus-per-task=75
+#SBATCH --job-name="lottery_simulation_6_nonrandom"
 #SBATCH --mail-user=cowzin@snu.ac.kr
 #SBATCH --mail-type=ALL
 #SBATCH --output=out/py-%x.%j.out
 #SBATCH --error=out/py-%x.%j.err
 
 module python3
-srun python3 /home/cowzin/code/simulation_model5_diffF.py
+srun python3 /home/cowzin/code/simulation_model6_nonrandom.py
