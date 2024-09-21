88e898715366e6f32cd924b2c6bcfba852fe60aa
woojin_99 <86119975+WOOZi-n@users.noreply.github.com>
2024-09-21 16:09:51 +0900

    240921_2

--- script_model2_common_w.sh
+++ script_model2_common_w.sh
@@ -5,11 +5,11 @@
 #SBATCH --nodelist=n05
 #SBATCH --ntasks=1 
 #SBATCH --cpus-per-task=75
-#SBATCH --job-name="lottery_simulation_4_2"
+#SBATCH --job-name="lottery_simulation2_common_w"
 #SBATCH --mail-user=cowzin@snu.ac.kr
 #SBATCH --mail-type=ALL
 #SBATCH --output=out/py-%x.%j.out
 #SBATCH --error=out/py-%x.%j.err
 
 module python3
-srun python3 /home/cowzin/code/simulation_model2_1.py
+srun python3 /home/cowzin/code/simulation_model2_common_w.py
